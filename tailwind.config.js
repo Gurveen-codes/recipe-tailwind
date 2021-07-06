6fb9c8bfc70df5cc591c0a1caa11405084be24e8
Gurveen <gurveens11@gmail.com>
2021-07-06 17:20:27 +0530

    Intial Project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,23 @@
 module.exports = {
-  mode: 'jit',
-  purge: [
-    './public/**/*.html'
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	mode: 'jit',
+	purge: ['./public/**/*.html'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				primary: '#ff6363',
+				secondary: {
+					100: '#e2e2d5',
+					200: '#888883',
+				},
+			},
+			fontFamily: {
+				poppins: ['Poppins', 'sans-serif'],
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 }
